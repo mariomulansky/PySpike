7ff5a4afe2d7a40dce34ae187a23b7d0feba33ba
Mario Mulansky <mario.mulansky@gmx.net>
2014-10-16 15:26:58 +0200

    catch ImportError in setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,23 @@
 
 Handles the compilation of pyx source files
 
+run as:
+python setup.py build_ext --inplace
+
 Copyright 2014, Mario Mulansky <mario.mulansky@gmx.net>
 
 Distributed under the BSD License
 
 """
 from distutils.core import setup
-from Cython.Build import cythonize
 import numpy
 
-setup(
-    ext_modules=cythonize("pyspike/*.pyx"),
-    include_dirs=[numpy.get_include()]
-)
+try:
+    from Cython.Build import cythonize
+    setup(
+        ext_modules=cythonize("pyspike/*.pyx"),
+        include_dirs=[numpy.get_include()]
+    )
+except ImportError:
+    print("Error: Cython is not installed! You will only be able to use the \
+much slower Python backend in PySpike.")
