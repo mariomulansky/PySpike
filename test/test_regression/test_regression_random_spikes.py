94c5fd007d33a38f3c9d1121749cb6ffb162394c
Mario Mulansky <mario.mulansky@gmx.net>
2015-12-19 13:05:23 +0100

    python3 print function in new regression test

--- test/test_regression/test_regression_random_spikes.py
+++ test/test_regression/test_regression_random_spikes.py
@@ -4,6 +4,8 @@ Copyright 2015, Mario Mulansky <mario.mulansky@gmx.net>
 
 Distributed under the BSD License
 """
+from __future__ import print_function
+
 import numpy as np
 from scipy.io import loadmat
 import pyspike as spk
@@ -63,10 +65,10 @@ def check_regression_dataset(spike_file="benchmark.mat",
         # spike_sync = spk.spike_sync_multi(spike_trains)
 
         if abs(isi - results_cSPIKY[i][0]) > 1E-14:
-            print "Error in ISI:", i, isi, results_cSPIKY[i][0]
-            print "Spike trains:"
+            print("Error in ISI:", i, isi, results_cSPIKY[i][0])
+            print("Spike trains:")
             for st in spike_trains:
-                print st.spikes
+                print(st.spikes)
 
         err = abs(spike - results_cSPIKY[i][1])
         if err > 1E-14:
@@ -75,35 +77,39 @@ def check_regression_dataset(spike_file="benchmark.mat",
             err_max = err
             err_max_ind = i
 
-    print "Total Errors:", err_count
+    print("Total Errors:", err_count)
 
     if err_max_ind > -1:
-        print "Max SPIKE distance error:", err_max, "at index:", err_max_ind
+        print("Max SPIKE distance error:", err_max, "at index:", err_max_ind)
         spike_train_data = spike_train_sets[err_max_ind]
         for spikes in spike_train_data[0]:
-            print spikes.flatten()
+            print(spikes.flatten())
 
 
 def check_single_spike_train_set(index):
     """ Debuging function """
     np.set_printoptions(precision=15)
+    spike_file = "regression_random_spikes.mat"
+    spikes_name = "spikes"
+    result_name = "Distances"
+    result_file = "regression_random_results_cSPIKY.mat"
 
-    spike_train_sets = loadmat("benchmark.mat")['spikes'][0]
+    spike_train_sets = loadmat(spike_file)[spikes_name][0]
 
-    results_cSPIKY = loadmat("results_cSPIKY.mat")['Distances']
+    results_cSPIKY = loadmat(result_file)[result_name]
 
     spike_train_data = spike_train_sets[index]
 
     spike_trains = []
     for spikes in spike_train_data[0]:
-        print "Spikes:", spikes.flatten()
+        print("Spikes:", spikes.flatten())
         spike_trains.append(spk.SpikeTrain(spikes.flatten(), 100.0))
 
-    print spk.spike_distance_multi(spike_trains)
+    print(spk.spike_distance_multi(spike_trains))
 
-    print results_cSPIKY[index][1]
+    print(results_cSPIKY[index][1])
 
-    print spike_trains[1].spikes
+    print(spike_trains[1].spikes)
 
 
 if __name__ == "__main__":
