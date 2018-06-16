7880aba982680e8b32e191fbab4e5f0598c09db1
Jonathan Jouty <me@jonathanjouty.com>
2018-06-16 16:25:45 +0100

    Add tests for PieceWiseConstFunc.integral that should raise AssertionErrors

--- test/test_function.py
+++ test/test_function.py
@@ -10,6 +10,7 @@ Distributed under the BSD License
 from __future__ import print_function
 import numpy as np
 from copy import copy
+from nose.tools import raises
 from numpy.testing import assert_equal, assert_almost_equal, \
     assert_array_equal, assert_array_almost_equal
 
@@ -139,6 +140,30 @@ def test_pwc_integral():
     # test part interval, between two edges
     assert_equal(f1.integral((1.0,2.0)), 1.0*-0.5)
     assert_equal(f1.integral((1.2,1.7)), (1.7-1.2)*-0.5)
+
+@raises(AssertionError)
+def test_pwc_integral_bad_bounds_inv():
+    # some random data
+    x = [0.0, 1.0, 2.0, 2.5, 4.0]
+    y = [1.0, -0.5, 1.5, 0.75]
+    f1 = spk.PieceWiseConstFunc(x, y)
+    f1.integral((3,2))
+
+@raises(AssertionError)
+def test_pwc_integral_bad_bounds_oob_1():
+    # some random data
+    x = [0.0, 1.0, 2.0, 2.5, 4.0]
+    y = [1.0, -0.5, 1.5, 0.75]
+    f1 = spk.PieceWiseConstFunc(x, y)
+    f1.integral((1,6))
+
+@raises(AssertionError)
+def test_pwc_integral_bad_bounds_oob_2():
+    # some random data
+    x = [0.0, 1.0, 2.0, 2.5, 4.0]
+    y = [1.0, -0.5, 1.5, 0.75]
+    f1 = spk.PieceWiseConstFunc(x, y)
+    f1.integral((-1,3))
 
 
 def test_pwl():
