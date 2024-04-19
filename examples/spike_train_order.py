0a7db8cca6ec514b8069a9e2581cf9bae242491c
KevinLin19 <150128883+KevinLin19@users.noreply.github.com>
2024-04-19 16:50:30 +0200

    Add files via upload

--- examples/spike_train_order.py
+++ examples/spike_train_order.py
@@ -6,9 +6,9 @@ import pyspike as spk
 st1 = spk.generate_poisson_spikes(1.0, [0, 20])
 st2 = spk.generate_poisson_spikes(1.0, [0, 20])
 
-d = spk.spike_directionality(st1, st2)
+d = spk.spike_order(st1, st2)
 
-print "Spike Directionality of two Poissonian spike trains:", d
+print "Spike Order of two Poissonian spike trains:", d
 
 E = spk.spike_train_order_profile(st1, st2)
 
@@ -31,7 +31,7 @@ F_init = spk.spike_train_order(spike_trains)
 
 print "Initial Synfire Indicator for 20 Poissonian spike trains:", F_init
 
-D_init = spk.spike_directionality_matrix(spike_trains)
+D_init = spk.spike_order_matrix(spike_trains)
 
 phi, _ = spk.optimal_spike_train_sorting(spike_trains)
 
@@ -43,10 +43,10 @@ D_opt = spk.permutate_matrix(D_init, phi)
 
 plt.figure()
 plt.imshow(D_init)
-plt.title("Initial Directionality Matrix")
+plt.title("Initial Order Matrix")
 
 plt.figure()
 plt.imshow(D_opt)
-plt.title("Optimized Directionality Matrix")
+plt.title("Optimized Order Matrix")
 
 plt.show()
