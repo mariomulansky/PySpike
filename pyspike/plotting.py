6a7ef8c8bac4279ca561543a07f97f1123679842
KevinLin19 <150128883+KevinLin19@users.noreply.github.com>
2024-06-25 16:51:04 +0200

    add a function to plot spike matching

--- pyspike/plotting.py
+++ pyspike/plotting.py
@@ -3,6 +3,7 @@ import pyspike as spk
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 from pyspike.generate_surrogate import generate_surro
+from pyspike.latency_correction import Spike_time_difference_matrix
 
 
 def Multi_Profile(spike_trains, variable):
@@ -123,7 +124,7 @@ def f_all_trains(spikes):
         start_idx = sum(dummy[0:trc+1])
         end_idx = start_idx + num_spikes_per_train[trc]
         all_indy[start_idx:end_idx] = [trc+1] * num_spikes_per_train[trc]
-   
+
     sp_flat = [spike for train in spikes for spike in train]
     sp_indy = sorted(range(len(sp_flat)), key=lambda i: sp_flat[i])
     all_trains = [all_indy[idx] for idx in sp_indy]
@@ -142,7 +143,8 @@ def plot_matrix(matrix, variable, variable_value=None, showing=0):
                      2 for SPIKE-distance,
                      3 for Spike-Synchro,
                      4 for SPIKE-Order,
-                     5 for Spike train order.
+                     5 for Spike train order
+                     6 for Spike time difference.
     :type variable: int
     :param variable_value: The value associated with the variable. If None, it's not displayed.
     :type variable_value: float or None
@@ -150,7 +152,7 @@ def plot_matrix(matrix, variable, variable_value=None, showing=0):
     :type showing: int, optional
     """
 
-    possible_variable = ['ISI-distance', 'SPIKE-distance', 'Spike-Synchro', 'SPIKE-Order', 'Spike train order']
+    possible_variable = ['ISI-distance', 'SPIKE-distance', 'Spike-Synchro', 'SPIKE-Order', 'Spike train order', 'Spike time difference']
     
     num_trains = len(matrix)
     if showing == 1:
@@ -173,7 +175,6 @@ def plot_matrix(matrix, variable, variable_value=None, showing=0):
     plt.yticks(np.arange(num_trains),np.arange(num_trains)+1, fontsize=14)
     plt.jet()
     plt.colorbar()
-    plt.show()
 
 def plot_profile(x_prof, y_prof, variable, variable_value=None, showing=0):
     """
@@ -234,7 +235,6 @@ def plot_profile(x_prof, y_prof, variable, variable_value=None, showing=0):
     
     plt.xlabel('Time', color='k', fontsize=18)
     plt.ylabel("%s" %(possible_variable[variable-1]), color='k', fontsize=18)
-    plt.show()
 
 def plot_spike_trains(spikes, tmin, tmax, phi=None, showing=0, order_color=0):
     """
@@ -250,6 +250,7 @@ def plot_spike_trains(spikes, tmin, tmax, phi=None, showing=0, order_color=0):
     :type order_color: int, optional
     """
     num_trains = len(spikes)
+    plotted_spikes = []
 
     if showing == 1:
         for i in range(num_trains):
@@ -257,14 +258,14 @@ def plot_spike_trains(spikes, tmin, tmax, phi=None, showing=0, order_color=0):
             for j in range(len(spikes[i])):
                 print("%i %.8f" % (j+1, spikes[i][j]))
         print("\n")
-
+        
     if order_color == 1:
         D = spk.spike_order_values(spikes)
         colors = [(0, 0, 0.5), (0, 0, 1), (0, 1, 1), (0, 1, 0), (1, 1, 0), (1, 0.5, 0), (1, 0, 0), (0.5, 0, 0)]
         positions = [0.0, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0]
         cm = LinearSegmentedColormap.from_list('custom_cmap', list(zip(positions, colors)), N=256)
         fig, ax = plt.subplots(figsize=(17, 10), dpi=80)
-        plt.colorbar(plt.cm.ScalarMappable(cmap=cm, norm=plt.Normalize(vmin=-1, vmax=1)), ax=ax)
+        plotted_spikes.append(plt.colorbar(plt.cm.ScalarMappable(cmap=cm, norm=plt.Normalize(vmin=-1, vmax=1)), ax=ax))
 
         C = Multi_Profile(spikes, 1)[1]
         order = f_all_trains(spikes)[0]
@@ -293,10 +294,10 @@ def plot_spike_trains(spikes, tmin, tmax, phi=None, showing=0, order_color=0):
     else:
         plt.yticks(np.arange(num_trains)+1, reversed([x+1 for x in phi]), fontsize=14)
 
-    plt.plot((tmin, tmax), (0.5, 0.5), ':', color='k', linewidth=1)
-    plt.plot((tmin, tmax), (num_trains+0.5, num_trains+0.5), ':', color='k', linewidth=1)
-    plt.plot((tmin, tmin), (0.5, num_trains+0.5), ':', color='k', linewidth=1)
-    plt.plot((tmax, tmax), (0.5, num_trains+0.5), ':', color='k', linewidth=1)
+    plotted_spikes.append(plt.plot((tmin, tmax), (0.5, 0.5), ':', color='k', linewidth=1))
+    plotted_spikes.append(plt.plot((tmin, tmax), (num_trains+0.5, num_trains+0.5), ':', color='k', linewidth=1))
+    plotted_spikes.append(plt.plot((tmin, tmin), (0.5, num_trains+0.5), ':', color='k', linewidth=1))
+    plotted_spikes.append(plt.plot((tmax, tmax), (0.5, num_trains+0.5), ':', color='k', linewidth=1))
 
     N = 0
     if phi == None:
@@ -305,11 +306,11 @@ def plot_spike_trains(spikes, tmin, tmax, phi=None, showing=0, order_color=0):
                 if order_color == 1:
                     color = cm((D[i][j] + 1) / 2)
                     if sorted_C[N] == 0:
-                        plt.plot((spikes[i][j], spikes[i][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1)
+                        plotted_spikes.append(plt.plot((spikes[i][j], spikes[i][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1))
                     else:
-                        plt.plot((spikes[i][j], spikes[i][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color=color, linewidth=1+2*sorted_C[N])
+                        plotted_spikes.append(plt.plot((spikes[i][j], spikes[i][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color=color, linewidth=1+2*sorted_C[N]))
                 else:
-                    plt.plot((spikes[i][j], spikes[i][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1)
+                    plotted_spikes.append(plt.plot((spikes[i][j], spikes[i][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1))
                 N += 1
     else:
         for i in range(num_trains):
@@ -317,14 +318,181 @@ def plot_spike_trains(spikes, tmin, tmax, phi=None, showing=0, order_color=0):
                 if order_color == 1:
                     color = cm((D[phi[i]][j] + 1) / 2)
                     if sorted_C[N] == 0:
-                        plt.plot((spikes[phi[i]][j], spikes[phi[i]][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1)
+                        plotted_spikes.append(plt.plot((spikes[phi[i]][j], spikes[phi[i]][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1))
                     else:
-                        plt.plot((spikes[phi[i]][j], spikes[phi[i]][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color=color, linewidth=1+2*sorted_C[N])
+                        plotted_spikes.append(plt.plot((spikes[phi[i]][j], spikes[phi[i]][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color=color, linewidth=1+2*sorted_C[N]))
                 else:
-                    plt.plot((spikes[phi[i]][j], spikes[phi[i]][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1)
+                    plotted_spikes.append(plt.plot((spikes[phi[i]][j], spikes[phi[i]][j]), (num_trains-i+0.5, num_trains-i-.5), '-', color='k', linewidth=1))
                 N += 1
-    plt.show()
+    return fig, ax, plotted_spikes
+
+def spike_matching_plot(spikes, tmin, tmax):
+    num_trains = len(spikes)
+    yticklabel1, yticklabel2, yticklabel3, num_spikes = [], [], [], []
+    STDM = Spike_time_difference_matrix(spikes)
+    for i in range(num_trains):
+        num_spikes.append(len(spikes[i]))
+        yticklabel1.append(f'{num_trains-i}')
+        yticklabel2.append(f'{num_trains-i}')
+        yticklabel3.append(f'{num_trains-i}')
+        if i == 0:
+            yticklabel3.append('')
+        if i == num_trains//2-1:
+            yticklabel2.append('')
+        if i == num_trains//2:
+            yticklabel2.append('')
+        if i == num_trains-2:
+            yticklabel1.append('')
+
+    # Plotting matches with spike train #1
+    fig, ax, plotted_spikes = plot_spike_trains(spikes, tmin, tmax, order_color=1)
+    y_positions = np.arange(num_trains+1)
+
+    for i, line in enumerate(plotted_spikes):
+        if i > 4 + num_spikes[0]:
+            line[0].set_ydata([y-1 for y in line[0].get_ydata().tolist()])
     
+    ax.set_title("Matches with Spike Train #1", color='k', fontsize=24)
+    plt.plot((tmin, tmax), (num_trains-1, num_trains-1), ':', color='k', linewidth=2)
+    for i in range(1, num_trains):
+        time, e_prof = Multi_Profile([spikes[0],spikes[i]], 3)
+        j = 0
+        while j < len(time)-1:
+            if e_prof[j] == 1:
+                plt.plot((time[j], time[j+1]), (num_trains-i-1, num_trains-i-1), '-', color='b', linewidth=2)
+                j += 2
+            elif e_prof[j] == -1:
+                plt.plot((time[j], time[j+1]), (num_trains-i-1, num_trains-i-1), '-', color='r', linewidth=2)
+                j += 2
+            else:
+                j += 1
+
+    data = STDM[0]
+    max_data_value = max(abs(min(data)), abs(max(data)))
+    right_ax = adjust_axes(fig, ax, plotted_spikes, max_data_value, y_positions, [-1, num_trains+1], yticklabel1)
+    right_ax.plot((-max_data_value, max_data_value), (num_trains-1, num_trains-1), ':', color='k', linewidth=2)
+    for i in range(num_trains):
+        value = data[num_trains-i-1]
+        color = 'blue' if value > 0 else 'red'
+        right_ax.barh(y_positions[i], value, height=1, color=color, edgecolor='black')
+
+    # Plotting matches with spike train #{num_trains//2}
+    fig, ax, plotted_spikes = plot_spike_trains(spikes, tmin, tmax, order_color=1)
+
+    for i, line in enumerate(plotted_spikes):
+        if i > 4 + sum(num_spikes[:num_trains//2]):
+            line[0].set_ydata([y-2 for y in line[0].get_ydata().tolist()])
+        elif i > 4 + sum(num_spikes[:num_trains//2-1]):
+            line[0].set_ydata([y-1 for y in line[0].get_ydata().tolist()])
+
+    ax.set_title(f"Matches with Spike Train #{num_trains//2}", color='k', fontsize=24)
+    plt.plot((tmin, tmax), (num_trains//2-1, num_trains//2-1), ':', color='k', linewidth=2)
+    plt.plot((tmin, tmax), (num_trains//2+1, num_trains//2+1), ':', color='k', linewidth=2)
+    for i in range(num_trains//2-1):
+        time, e_prof = Multi_Profile([spikes[num_trains//2-1],spikes[i]], 3)
+        j = 0
+        while j < len(time)-1:
+            if e_prof[j] == 1:
+                plt.plot((time[j], time[j+1]), (num_trains-i, num_trains-i), '-', color='r', linewidth=2)
+                j += 2
+            elif e_prof[j] == -1:
+                plt.plot((time[j], time[j+1]), (num_trains-i, num_trains-i), '-', color='b', linewidth=2)
+                j += 2
+            else:
+                j += 1
+    for i in range(num_trains//2, num_trains):
+        time, e_prof = Multi_Profile([spikes[num_trains//2-1],spikes[i]], 3)
+        j = 0
+        while j < len(time)-1:
+            if e_prof[j] == 1:
+                plt.plot((time[j], time[j+1]), (num_trains-i-2, num_trains-i-2), '-', color='b', linewidth=2)
+                j += 2
+            elif e_prof[j] == -1:
+                plt.plot((time[j], time[j+1]), (num_trains-i-2, num_trains-i-2), '-', color='r', linewidth=2)
+                j += 2
+            else:
+                j += 1
+
+    data = STDM[num_trains//2-1]
+    max_data_value = max(abs(min(data)), abs(max(data)))
+    y_positions = []
+    for i in range(num_trains+2):
+        if not (i == num_trains//2 or i == num_trains//2+1):
+            y_positions.append(i-1)
+    right_ax = adjust_axes(fig, ax, plotted_spikes, max_data_value, np.arange(-1, num_trains+1), [-2, num_trains+1], yticklabel2, matching=2)
+    for i in range(num_trains):
+        value = data[num_trains-i-1]
+        if value != 0:
+            color = 'blue' if value > 0 else 'red'
+            if num_trains-i-1 < num_trains//2-1:
+                right_ax.barh(y_positions[i], -value, height=1, color=color, edgecolor='black')
+            else:
+                right_ax.barh(y_positions[i], value, height=1, color=color, edgecolor='black')
+    right_ax.plot((-max_data_value, max_data_value), (num_trains//2-1, num_trains//2-1), ':', color='k', linewidth=2)
+    right_ax.plot((-max_data_value, max_data_value), (num_trains//2+1, num_trains//2+1), ':', color='k', linewidth=2)
+    
+    # Plotting matches with spike train #{num_trains}
+    fig, ax, plotted_spikes = plot_spike_trains(spikes, tmin, tmax, order_color=1)
+    y_positions = np.arange(num_trains+1)
+    
+    for i, line in enumerate(plotted_spikes):
+        if i >= len(plotted_spikes)-num_spikes[-1]:
+            line[0].set_ydata([y-1 for y in line[0].get_ydata().tolist()])
+
+    ax.set_title(f"Matches with Spike Train #{num_trains}", color='k', fontsize=24)
+    plt.plot((tmin, tmax), (1, 1), ':', color='k', linewidth=2)
+    for i in range(num_trains-1):
+        time, e_prof = Multi_Profile([spikes[num_trains-1],spikes[i]], 3)
+        j = 0
+        while j < len(time)-1:
+            if e_prof[j] == 1:
+                plt.plot((time[j], time[j+1]), (num_trains-i, num_trains-i), '-', color='r', linewidth=2)
+                j += 2
+            elif e_prof[j] == -1:
+                plt.plot((time[j], time[j+1]), (num_trains-i, num_trains-i), '-', color='b', linewidth=2)
+                j += 2
+            else:
+                j += 1
+
+    data = STDM[-1]
+    max_data_value = max(abs(min(data)), abs(max(data)))
+    right_ax = adjust_axes(fig, ax, plotted_spikes, max_data_value, y_positions, [-1, num_trains+1], yticklabel3)
+    for i in range(num_trains):
+        value = data[num_trains-i-1]
+        if value != 0:
+            color = 'blue' if value > 0 else 'red'
+            right_ax.barh(y_positions[i]+1, -value, height=1, color=color, edgecolor='black')
+    right_ax.plot((-max_data_value, max_data_value), (1, 1), ':', color='k', linewidth=2)
+
+def adjust_axes(fig, ax, plotted_spikes, max_data_value, y_positions, y_lim, yticklabel, matching=1):
+    if matching == 1:
+        num_trains = len(y_positions)-1
+        y = -0.5
+    else:
+        num_trains = len(y_positions)-2
+        y = -1.5
+        
+    plotted_spikes[0].remove()
+    plotted_spikes[1][0].set_ydata([y, y])
+    plotted_spikes[3][0].set_ydata([y, num_trains+0.5])
+    plotted_spikes[4][0].set_ydata([y, num_trains+0.5])
+
+    ax.set_yticks(y_positions)
+    ax.set_ylim(y_lim[0], y_lim[1])
+    ax.set_yticklabels(yticklabel)
+    
+    right_ax = fig.add_axes([0.8, 0.11, 0.15, 0.77])
+    right_ax.set_xlim([-max_data_value, max_data_value])
+    right_ax.invert_yaxis()
+    right_ax.axvline(0, color='black', linewidth=1)
+    right_ax.yaxis.tick_right()
+    right_ax.set_yticks(y_positions)
+    right_ax.set_ylim(y_lim[0], y_lim[1])
+    right_ax.set_yticklabels(yticklabel)
+    right_ax.set_title("Leaders   Followers", fontsize=18)
+    right_ax.set_xlabel("<Spike Time Difference>", fontsize=18)
+    return right_ax
+
 def plot_surrogates(spike_trains, num_surros):
     """
     Plots the surrogate distribution of spike train order values and the optimal spike train order value.
@@ -382,4 +550,19 @@ def plot_surrogates(spike_trains, num_surros):
         p = 1/(num_surros+1)
         plt.title("z = %.8f ; p >> %.8f" %(z, p), color='k', fontsize=24)
     plt.legend()
-    plt.show()
+
+def plot_average_diagonal_value(STDM):
+    num_trains = len(STDM)
+    average_diagonal_values = []
+    for i in range(num_trains):
+        value = 0
+        for j in range(num_trains-i):
+                value += STDM[j][j+i]
+        average_diagonal_values.append(value/(num_trains-i))
+
+    plt.figure(figsize=(17, 10), dpi=80)
+    plt.plot(average_diagonal_values, '-kx', linewidth=3, markersize=10, markeredgewidth=2)
+    plt.title("Average diagonal value of STDM", color='k', fontsize=24)
+    plt.xlabel('STDM value', fontsize=18)
+    plt.ylabel('Average value', fontsize=18)
+    plt.grid()
